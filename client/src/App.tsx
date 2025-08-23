bb1e457e28d30d2f7b3cf871bf86f9b0e52ef13d
martinvega2003 <josemartinvt2003@gmail.com>
2025-08-23 14:53:37 -0300

    chore(client): setup dark & light mode context with TailwindCSS and context api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from './context/ThemeModeContext'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <ThemeProvider>
+      <Navbar />
+      <div className='bg-white dark:bg-gray-800'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ ThemeProvider>
   )
 }
 
