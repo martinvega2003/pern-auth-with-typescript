825ebb1f843822f26c1c97e82678b673827c020f
martinvega2003 <josemartinvt2003@gmail.com>
2025-08-26 14:16:51 -0300

    chore(App.tsx): Add routes and Initial page in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import { ThemeProvider } from './context/ThemeModeContext'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Pages and Components
+import InitialPage from './pages/InitialPage'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <ThemeProvider>
-      <div></div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<InitialPage />} />
+          <Route path='/about' element={<div>About</div>} />
+        </Routes>
+      </BrowserRouter>
     </ ThemeProvider>
   )
 }
