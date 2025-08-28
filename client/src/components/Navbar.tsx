cf04056263e991f55a247cd901a0cb862f4a488a
martinvega2003 <josemartinvt2003@gmail.com>
2025-08-28 13:07:26 -0300

    chore(Navbar): Update Navbar stylying for dark and light themes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useTheme } from "../context/ThemeModeContext";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Navbar: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <nav className="fixed p-4 bg-gray-200 dark:bg-gray-800 flex justify-between border-b border-gray-300 dark:border-gray-700 w-full">
-      <span className="text-xl">My App</span>
+      <span className="text-xl dark:text-white">My App</span>
       <button
         onClick={toggleTheme}
-        className="px-3 py-1 rounded bg-gray-300 dark:bg-gray-600"
+        className="bg-gray-300 dark:bg-gray-600 dark:text-white px-3 py-1 rounded-md cursor-pointer"
       >
-        {theme === "light" ? "🌙 Dark" : "☀️ Light"}
+        {theme === "light" ? <p className="flex justify-start items-center gap-2"><FaMoon /> Dark</p> : <p className="flex justify-start items-center gap-2"><FaSun /> Light</p>}
       </button>
     </nav>
   );
