2c1a91442f39b9a8a99991bfce6a070789981f9d
martinvega2003 <josemartinvt2003@gmail.com>
2025-09-04 12:02:06 -0300

    feat: attach JWT token to API requests using axios interceptor

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -7,4 +7,13 @@ const api = axios.create({
   },
 });
 
+// Attach token to every request if it exists
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
