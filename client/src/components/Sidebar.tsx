5d4f29cdaaa1192212b585c494fd3709a0a87431
martinvega2003 <josemartinvt2003@gmail.com>
2025-09-08 10:38:14 -0300

    fix(sidebar): Fix sidebar links bg color overflow and use NavLink from react-router-dom to add styling when a link is active

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import Button from "./Button";
 import { FaArrowRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Sidebar: React.FC = () => {
   const { isAuthenticated, logout } = useAuth()!;
@@ -23,17 +23,24 @@ const Sidebar: React.FC = () => {
   ];
 
   const SidebarLink = ({ to, label }: SidebarLinkProps) => (
-    <Link
+    <NavLink
       to={to}
-      className="w-full h-fit m-2 px-4 py-2 hover:bg-gray-300 dark:hover:bg-gray-700 rounded-md transition-colors duration-300 block"
+      className={({ isActive }) => 
+        `w-60 h-fit m-2 px-4 py-2 rounded-md transition-colors duration-300 block
+        ${!isOpen ? "bg-transparent" : 
+        isActive ? 
+          "bg-gray-300 dark:bg-gray-700 font-semibold" : 
+          "font-normal hover:bg-gray-300 dark:hover:bg-gray-700"
+        }`
+      }
       onClick={() => setIsOpen(false)} // Close sidebar on link click
     >
       {label}
-    </Link>
+    </NavLink>
   );
 
   return (
-    <div className={`fixed top-16 left-0 h-full bg-zinc-100 dark:bg-gray-900 dark:text-white transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-3/4'} w-64`}>
+    <div className={`fixed top-16 left-0 w-64 h-full bg-zinc-100 dark:bg-gray-900 dark:text-white transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-52'}`}>
       <div className="p-4 flex justify-between items-center border-b border-gray-700">
         <h2 className="text-lg font-semibold">Sidebar</h2>
         <Button
@@ -44,12 +51,10 @@ const Sidebar: React.FC = () => {
           {isOpen ? <FaArrowRight className="rotate-180 transition-transform duration-500" /> : <FaArrowRight className="rotate-0 transition-transform duration-500" /> }
         </Button>
       </div>
-      <nav>
+      <nav className="w-full h-full overflow-y-auto">
         <ul>
           {links.map((link) => (
-            <li key={link.to}>
               <SidebarLink {...link} />
-            </li>
           ))}
           <li>
             <Button 
